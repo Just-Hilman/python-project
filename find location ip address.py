f89f82e928094a89b918ae83e189a3224e770f9a
Just-Hilman <hilman620@yahoo.com>
2023-04-24 19:02:07 +0700

    Update find location ip address.py

--- find location ip address.py
+++ find location ip address.py
@@ -11,3 +11,8 @@ def printRecord(tgt):
     lat = rec['latitude']
     print("[*] Target" + tgt + "Geolocated")
     print("[+] City :", str(city) + " " + ", Country :", str(country) + " " + ", Continent :", str(cont) + " ")
+    print("Latitude :", str(lat) + " " + ", Longitude :", str(long))
+data = json.loads(urllib.request.urlopen("http://ip.jsontest.com/").read())
+tgt = data["ip"]
+print("IP Address found : ", tgt)
+printRecord(tgt)

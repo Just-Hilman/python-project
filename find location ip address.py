4cd81d58ce92b86f44b538a57bba8222c463d724
Just-Hilman <hilman620@yahoo.com>
2023-04-24 18:54:11 +0700

    Update find location ip address.py

--- find location ip address.py
+++ find location ip address.py
@@ -4,3 +4,7 @@ import urllib.request, json
 gi = pygeoip.GeoIP('GeoLiteCity.dat')
 def printRecord(tgt):
     rec = gi.record_by_name(tgt)
+    city = rec['city']
+    cont = rec['continent']
+    country = rec['country_name']
+    long = rec['longitude']

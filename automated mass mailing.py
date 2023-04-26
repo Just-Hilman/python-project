46fec29df74eabf0a12fedb66dc0f98c9ca5d9ca
Just-Hilman <hilman620@yahoo.com>
2023-04-26 20:09:12 +0700

    Update automated mass mailing.py

--- automated mass mailing.py
+++ automated mass mailing.py
@@ -2,3 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
 import time
+
+email_data = pd.read_excel("email_data.xlsx")
+
+# Replace with gmail credentials
+email_id = list(email_data["sender_email"])
+password = list(email_data["password"])
+
+# Replace with email recipients
+to_email = list(email_data["recipient_email"])
+
+# Count of sent email
+sent_email = []

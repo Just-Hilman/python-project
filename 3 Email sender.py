65ec07eebd3cb9ce5e1937ea9a938d142947e063
Just-Hilman <hilman620@yahoo.com>
2023-05-06 23:26:58 +0700

    Update 3 Email sender.py

--- 3 Email sender.py
+++ 3 Email sender.py
@@ -1,4 +1,16 @@
 from email.message import EmailMessage
 
 
-email_sender = ''
+email_sender = ""
+email_password = ""
+
+email_receiver = ""
+
+subject = "Test"
+body = "" "Test body email" ""
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['Subject'] = subject
+em.set_content(body)

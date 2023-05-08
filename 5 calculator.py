4debd85fedf9da76f83374ed559c73c918917206
Just-Hilman <64965507+Just-Hilman@users.noreply.github.com>
2023-05-08 20:05:43 +0700

    Update 5 calculator.py

--- 5 calculator.py
+++ 5 calculator.py
@@ -1,1 +1,16 @@
 # define the functions needed
+def add(a, b):
+    answer = a + b
+    print(str(a) + "+" + str(b) + "=" + str(answer))
+
+def sub(a, b):
+    answer = a - b
+    print(str(a) + "-" + str(b) + "=" + str(answer))
+
+def mul(a, b):
+    answer = a * b
+    print(str(a) + "*" + str(b) + "=" + str(answer))
+
+def div(a, b):
+    answer = a / b
+    print(str(a) + "/" + str(b) + "=" + str(answer))

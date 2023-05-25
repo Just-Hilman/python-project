23f1a778f7bfc665935b8ed330f1b37f12001622
Just-Hilman <64965507+Just-Hilman@users.noreply.github.com>
2023-05-25 22:44:12 +0700

    Update 8 quiz program.py

---
+++ 8 quiz program.py
@@ -0,0 +1,30 @@
+quiz = {
+    "question1": {
+        "question": "What is the capital of France?",
+        "answer": "Paris"
+    },
+    "question2": {
+        "question": "What is the capital of Germany?",
+        "answer": "Berlin"
+    },
+    "question3": {
+        "question": "What is the capital of Italy?",
+        "answer": "Milan"
+    },
+    "question4": {
+        "question": "What is the capital of Sapin?",
+        "answer": "Madrid"
+    },
+    "question5": {
+        "question": "What is the capital of Portugal?",
+        "answer": "Lisbon"
+    },
+    "question6": {
+        "question": "What is the capital of Switzerland?",
+        "answer": "Bern"
+    },
+    "question7": {
+        "question": "What is the capital of Austria?",
+        "answer": "Vienna"
+    },
+}

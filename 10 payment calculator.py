40a5006334aa58fde7b9588423680c653364bd2a
Just-Hilman <hilman620@yahoo.com>
2023-05-31 20:35:22 +0700

    Update 10 payment calculator.py

---
+++ 10 payment calculator.py
@@ -0,0 +1,7 @@
+def main():
+    print("This is a monthly payment loan calculator")
+    print("")
+
+    principal = float(input("Input the loan amount: "))
+    apr = float(input("Input the annual interest rate: "))
+    years = int(input("Input amount of years: "))

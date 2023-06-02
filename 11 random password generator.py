845c84d8377fc51fe7cfec6798d8492d1a0405ce
Just-Hilman <hilman620@yahoo.com>
2023-06-02 22:03:40 +0700

    Update 11 random password generator.py

---
+++ 11 random password generator.py
@@ -0,0 +1,7 @@
+import string
+import random
+
+characters = list(string.ascii_letters + string.digits + "!@#$%^&*()")
+
+def generate_password():
+    password_length = int(input("How long would you like your password to be? "))

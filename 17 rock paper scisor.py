e5d6a7bb9edc7a2050fb0a5499447fe782cb359f
Just-Hilman <hilman620@yahoo.com>
2023-06-19 19:07:45 +0700

    Update 17 rock paper scisor.py

--- 17 rock paper scisor.py
+++ 17 rock paper scisor.py
@@ -1,4 +1,14 @@
 import random
 
-options = ["rock", "paper", "scisor"]
-user_input = input("Chose rock, paper, or scissors: ")
+exit = False
+user_points = 0
+computer_points = 0
+
+while exit == False:
+    options = ["rock", "paper", "scisor"]
+    user_input = input("Chose rock, paper, or scissors or exit: ")
+    computer_input = random.choice(options)
+
+    if user_input == "exit":
+        print("Game Ended")
+        exit = True

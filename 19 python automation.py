e64a9db58d7cfe1a56f82acc73b122203cc8004b
Just-Hilman <hilman620@yahoo.com>
2023-06-28 20:58:59 +0700

    Update 19 python automation.py

--- 19 python automation.py
+++ 19 python automation.py
@@ -11,3 +11,10 @@ def send_message():
         'key': 'textbelt'
     })
     print(resp.json())
+
+# schedule.every().day.at('06:00').do(send_message)
+schedule.every(10).seconds.do(send_message)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)

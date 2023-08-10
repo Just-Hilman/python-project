2ffc38311d7fd1be1e869e0b197f69c9784ad014
Just-Hilman <hilman620@yahoo.com>
2023-08-10 20:35:59 +0700

    Update 26 multiclipboard.py

--- 26 multiclipboard.py
+++ 26 multiclipboard.py
@@ -2,5 +2,14 @@ import sys
 import json
 import clipboard
 
-data = clipboard.paste()
-print(data)
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+
+    if command == "save":
+        print("save")
+    elif command == "load":
+        print("load")
+    elif command == "list":
+        print("list")
+    else:
+        print("unknown command")

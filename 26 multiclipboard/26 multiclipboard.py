92c8d037be44ebec004989ad8292e2f509842398
Just-Hilman <hilman620@yahoo.com>
2023-08-12 21:08:04 +0700

    update multiclipboard.py

--- 26 multiclipboard/26 multiclipboard.py
+++ 26 multiclipboard/26 multiclipboard.py
@@ -2,6 +2,11 @@ import sys
 import json
 import clipboard
 
+def save_items(filepath, data):
+    with open(filepath, "w") as f:
+        json.dump(data, f)
+save_items("test.json", {"key": "value"})
+
 if len(sys.argv) == 2:
     command = sys.argv[1]
 
@@ -13,3 +18,5 @@ if len(sys.argv) == 2:
         print("list")
     else:
         print("unknown command")
+else :
+    print("Please pass exactly one command.")

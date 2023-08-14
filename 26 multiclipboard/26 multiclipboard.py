79cf9bbfa8907e1d8cd73bf735bfca21c8cb34e2
Just-Hilman <hilman620@yahoo.com>
2023-08-14 20:17:41 +0700

    update multiclipboard.py

--- 26 multiclipboard/26 multiclipboard.py
+++ 26 multiclipboard/26 multiclipboard.py
@@ -5,7 +5,11 @@ import clipboard
 def save_items(filepath, data):
     with open(filepath, "w") as f:
         json.dump(data, f)
-save_items("test.json", {"key": "value"})
+
+def load_json(filepath):
+    with open(filepath, "r") as f:
+        data = json.load(f)
+        return data
 
 if len(sys.argv) == 2:
     command = sys.argv[1]

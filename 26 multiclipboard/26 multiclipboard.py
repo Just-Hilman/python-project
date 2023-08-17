ce1d18f0a46ce035a795a06f828d49606a771d06
Just-Hilman <hilman620@yahoo.com>
2023-08-17 21:06:52 +0700

    update

--- 26 multiclipboard/26 multiclipboard.py
+++ 26 multiclipboard/26 multiclipboard.py
@@ -2,20 +2,25 @@ import sys
 import json
 import clipboard
 
+SAVED_DATA = "clipboard.json"
+
 def save_items(filepath, data):
     with open(filepath, "w") as f:
         json.dump(data, f)
 
-def load_json(filepath):
+def load_items(filepath):
     with open(filepath, "r") as f:
         data = json.load(f)
         return data
 
 if len(sys.argv) == 2:
     command = sys.argv[1]
+    data = load_items(SAVED_DATA)
 
     if command == "save":
-        print("save")
+        key = input("Enter a key: ")
+        data[key] = clipboard.paste()
+        save_items(SAVED_DATA, data)
     elif command == "load":
         print("load")
     elif command == "list":

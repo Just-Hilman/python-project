1a958ce8a82b21cfdf7efce9db7640510c0a6bcb
Just-Hilman <hilman620@yahoo.com>
2023-08-22 14:49:07 +0700

    Update 27 weather fetcher.py

--- 27 weather fetcher.py
+++ 27 weather fetcher.py
@@ -4,4 +4,11 @@ API_KEY = ""
 BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
 
 city = input("Enter a city name: ")
-requests_url = f"{BASE_URL}?"
+requests_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(requests_url)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print("An error occured")

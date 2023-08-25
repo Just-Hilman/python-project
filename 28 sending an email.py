9d7f10c17ea633eb14770068d00e550e412d52e4
Just-Hilman <hilman620@yahoo.com>
2023-08-25 14:05:12 +0700

    Update 28 sending an email.py

--- 28 sending an email.py
+++ 28 sending an email.py
@@ -12,3 +12,14 @@ message = EmailMessage()
 message["From"] = sender_email
 message["To"] = receiver_email
 message["Subject"] = subject
+message.set_content(body)
+
+context = ssl.create_default_context()
+
+print("Sending Email")
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message.as_string())
+
+print("Success")

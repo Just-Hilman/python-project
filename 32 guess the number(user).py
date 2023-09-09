43225c38204940c49cc3fe605dec96598719d02e
Just-Hilman <hilman620@yahoo.com>
2023-09-09 19:27:39 +0700

    Update 32 guess the number(user).py

--- 32 guess the number(user).py
+++ 32 guess the number(user).py
@@ -15,4 +15,8 @@ def guess(x):
 def computer_guess(x):
     low = 1
     high = x
+    feedback = ''
+    while feedback != 'c':
+        guess = random.randint(low, high)
+        feedback = input(f'Is {guess} too high(H), too low(L), or correct(C)?? ').lower()
 guess(10)

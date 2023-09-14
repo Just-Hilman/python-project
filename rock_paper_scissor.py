0c4ea5cc7d4785300a4f15a7234f928aa25376fe
Just-Hilman <hilman620@yahoo.com>
2023-09-14 20:43:00 +0700

    Update rock_paper_scissor.py

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -10,6 +10,10 @@ def play():
     if is_win(user, computer):
         return 'You won'
     
+    return 'You lost'
+    
 def is_win(player, opponent):
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') or (player == 'p' and opponent == 'r'):
         return True
+    
+print(play())

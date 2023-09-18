89dc8e6bc9fe4588e9b6a26a0ad60a5715ff50fa
Just-Hilman <hilman620@yahoo.com>
2023-09-18 20:03:37 +0700

    Update hangman.py

--- 34 Hangman/hangman.py
+++ 34 Hangman/hangman.py
@@ -1,4 +1,15 @@
 import random
 from words import words
+import string
 
-print(words)
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in words:
+        word = random.choice(words)
+
+    return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)

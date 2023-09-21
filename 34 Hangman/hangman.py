413cb2b2dea483d617b18d6e9b085fa2f9fcd170
Just-Hilman <hilman620@yahoo.com>
2023-09-21 20:27:44 +0700

    Update hangman.py

--- 34 Hangman/hangman.py
+++ 34 Hangman/hangman.py
@@ -13,3 +13,14 @@ def hangman():
     word = get_valid_word(words)
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    # getting user input
+    user_letter = input("Guess a letter: ").upper()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+
+user_input = input("Type something: ")
+print(user_input)

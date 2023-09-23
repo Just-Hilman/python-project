d1d1626e50ff80d462570c6e3970ec1edf9f1a7e
Just-Hilman <hilman620@yahoo.com>
2023-09-23 19:18:00 +0700

    Update hangman.py

--- 34 Hangman/hangman.py
+++ 34 Hangman/hangman.py
@@ -17,13 +17,20 @@ def hangman():
 
     # getting user input
     while len(word_letters) > 0:
+        # letters used
+        # ' '.join(['a', 'b', 'cd'])--> 'a b cd'
+        print("You have used these letters: ", ' '.join(used_letters))
+
+        # what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+
         user_letter = input("Guess a letter: ").upper()
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
 
-        elif user_letter in used_letter:
+        elif user_letter in used_letters:
             print("You have already used that character. Please try again")
         
         else:

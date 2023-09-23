1741dccb4ebb624dd9df2c0b04d1fe7bf6a65ae8
Just-Hilman <hilman620@yahoo.com>
2023-09-23 19:08:07 +0700

    Update hangman.py

--- 34 Hangman/hangman.py
+++ 34 Hangman/hangman.py
@@ -16,17 +16,17 @@ def hangman():
     used_letters = set()
 
     # getting user input
-    user_letter = input("Guess a letter: ").upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+    while len(word_letters) > 0:
+        user_letter = input("Guess a letter: ").upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
 
-    elif user_letter in used_letter:
-        print("You have already used that character. Please try again")
-    
-    else:
-        print("Invalid character. Please try again")
+        elif user_letter in used_letter:
+            print("You have already used that character. Please try again")
+        
+        else:
+            print("Invalid character. Please try again")
 
-user_input = input("Type something: ")
-print(user_input)
+hangman()

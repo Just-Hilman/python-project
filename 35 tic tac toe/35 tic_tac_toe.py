4590f1125fc95e2448aa8fea1144ce8ab9e80398
Just-Hilman <hilman620@yahoo.com>
2023-10-01 19:48:34 +0700

    update

--- 35 tic tac toe/35 tic_tac_toe.py
+++ 35 tic tac toe/35 tic_tac_toe.py
@@ -15,7 +15,8 @@ class RandomComputerPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        square = random.choice(game.available_moves())
+        return square
 
 class HumanPlayer(Player):
     def __init__(self, letter):

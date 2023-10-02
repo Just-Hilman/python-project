92586f85c4c90710b44b53b547cf9392bf9fb558
Just-Hilman <hilman620@yahoo.com>
2023-10-02 20:14:28 +0700

    Update 35 tic_tac_toe.py

--- 35 tic tac toe/35 tic_tac_toe.py
+++ 35 tic tac toe/35 tic_tac_toe.py
@@ -23,4 +23,14 @@ class HumanPlayer(Player):
         super().__init__(letter)
 
     def get_move(self, game):
-        pass
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-9): ')
+            try:
+                val = int(square)
+                if val not in game.available_moves():
+                    raise ValueError
+                valid_square = True
+            except:
+                print("Invalid square. Try again")

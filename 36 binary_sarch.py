70f25bd8290c6b176707b5374292fe482d4d8ab4
Just-Hilman <hilman620@yahoo.com>
2023-10-26 20:46:18 +0700

    update

--- 36 binary_sarch.py
+++ 36 binary_sarch.py
@@ -3,3 +3,10 @@ def naive_search(l, target):
         if l[i] == target:
             return i
     return -1
+
+def binary_search(l, target):
+    midpoint = (len(l)) // 2
+    if l[midpoint] == target:
+        return midpoint
+    elif target < l[midpoint]:
+        return binary_search

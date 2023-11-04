bb46ed211758a6d5f9f684876d19fce2272b6ba5
Just-Hilman <hilman620@yahoo.com>
2023-11-04 19:34:15 +0700

    update

--- 37 minesweeper.py
+++ 37 minesweeper.py
@@ -1,3 +1,5 @@
+import random
+
 class Board:
     def __init__(self, dim_size, num_bombs):
         self.dim_size = dim_size
@@ -9,6 +11,14 @@ class Board:
 
     def make_new_board(self):
         board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+
+        bombs_planted = 0
+        while bombs_planted < self.numb_bombs:
+            loc = random.randint(0, self.dim_size**2 -1)
+            row = loc // self.dim_size
+            col = loc % self.dim_size
+
+            
 
 def play(dim_size=10, num_bombs=10):
     pass

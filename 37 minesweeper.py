7460ff17a746605204030773139240384232a04c
Just-Hilman <hilman620@yahoo.com>
2023-11-22 20:28:32 +0700

    update

--- 37 minesweeper.py
+++ 37 minesweeper.py
@@ -65,6 +65,11 @@ class Board:
                 else:
                     visible_board[row][col] = ' '
 
+        string_rep = ''
+        widths = []
+        for idx in range(self.dim_size):
+            columns = map(lambda x: x[idx], visible_board)
+
 def play(dim_size=10, num_bombs=10):
     board = Board(dim_size, num_bombs)
     pass

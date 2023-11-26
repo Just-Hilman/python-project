d4902b86713854996313f0dc0fdfc1b949498789
Just-Hilman <hilman620@yahoo.com>
2023-11-26 13:50:09 +0700

    update

--- 37 minesweeper.py
+++ 37 minesweeper.py
@@ -77,4 +77,6 @@ class Board:
 
 def play(dim_size=10, num_bombs=10):
     board = Board(dim_size, num_bombs)
-    pass
+    while len(board.dug) < board.dim_size ** 2 - num_bombs:
+        print(board)
+        user_input = re.split(',(\\s)*', input("Where would you like to dig? Input as row, col:"))

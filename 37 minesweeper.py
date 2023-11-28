39ae9bbbce06aeb06be9ab5f8e4b052d8091b87f
Just-Hilman <hilman620@yahoo.com>
2023-11-28 20:12:17 +0700

    update

--- 37 minesweeper.py
+++ 37 minesweeper.py
@@ -1,4 +1,5 @@
 import random
+import re
 
 class Board:
     def __init__(self, dim_size, num_bombs):
@@ -80,3 +81,7 @@ def play(dim_size=10, num_bombs=10):
     while len(board.dug) < board.dim_size ** 2 - num_bombs:
         print(board)
         user_input = re.split(',(\\s)*', input("Where would you like to dig? Input as row, col:"))
+        row, col = int(user_input[0]), int(user_input[-1])
+        if row < 0 or row >= board.dim_size or col < 0 or col >= dim_size:
+            print("Invalid location. Try again")
+            continue

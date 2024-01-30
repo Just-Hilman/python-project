c5424c9f397558769d711c059391e1ebf6a5e07e
Just-Hilman <justhilman409@gmail.com>
2024-01-30 20:13:01 +0700

    update

--- 41 Python project/main.py
+++ 41 Python project/main.py
@@ -38,7 +38,7 @@ def print_slot_machine(columns):
             if i != len(columns) -1:
                 print(column[row], "|")
             else:
-                print(column[row], "|")
+                print(column[row])
 
 def deposit():
     while True:
@@ -91,5 +91,8 @@ def main():
         else:
             break
     print(f"You are betting ${bet} on {lines} lines. Total bet is ${total_bet}")
+
+    slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 
 main()
